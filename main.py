dc52ec516520b4c0efac89e15774a0d7893819ab
alex <alexnakagama4@gmail.com>
2026-03-21 14:42:27 -0300

    Added configuration to env variables with dotenv and connection to database from archive .env

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from app.routers import users
+from app.core.database import engine
+from app.models.base import Base
+from app.models import user_model
+
+# Create the database tables
+Base.metadata.create_all(bind=engine)
 
 # Create a FastAPI instance
 app = FastAPI()
