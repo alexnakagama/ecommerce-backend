cd15510b88df0fe40d17521b2ed25452ef957dae
alex <alexnakagama4@gmail.com>
2026-03-23 13:47:34 -0300

    Refactor of user login, added new admin endpoints and created a pydantic model for the login requests

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from app.core.database import get_db
-from app.core.security import get_current_user, check_admin
+from app.core.security import check_admin
 from app.models.user_model import User
 from app.services.create_product import create_product as create_product_service
 from app.schemas.product.product_create import ProductCreate
@@ -67,7 +67,16 @@ async def create_product(product_data: ProductCreate, current_user: dict = Depen
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Missing product data")
     return create_product_service(name, description, price, db)
 
+# Endpoint to list all products
 @router.get("/products", summary="List all products", status_code=status.HTTP_200_OK)
 async def list_products(current_user: dict = Depends(check_admin), db: Session = Depends(get_db)):
     products = db.query(Product).all()
     return {"products": [ProductResponse.model_validate(product) for product in products]}
+
+# Endpoint to list a product by ID
+@router.get("/products/{product_id}", summary="List a product by ID", status_code=status.HTTP_200_OK)
+async def list_product_by_id(product_id: int, current_user: dict = Depends(check_admin), db: Session = Depends(get_db)):
+    product = db.query(Product).filter(Product.id == product_id).first()
+    if product is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Product was not found")
+    return ProductResponse.model_validate(product)
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,10 +1,12 @@
 from app.core.security import create_access_token, authenticate_user
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from app.core.database import get_db
+from app.models.user_model import User
 from app.services import create_user as create_user_service
 from app.schemas.user.user_create import UserCreate
+from app.schemas.user.login_request import LoginRequest
 
 # Create a router for user-related endpoints
 router = APIRouter(
@@ -18,10 +20,15 @@ router = APIRouter(
 async def create_user(user: UserCreate, db: Session = Depends(get_db)):
     return create_user_service.create_user(user.username, user.email, user.password, user.role, db)
 
-# Endpoint to login a user and return an access token
-@router.post("/login", summary="Login a user", status_code=status.HTTP_200_OK)
-async def login_user(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+# Endpoint for user login
+@router.post("/login")
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = authenticate_user(form_data.username, form_data.password, db)
-    access_token = create_access_token(user_data={"sub": user.username, "role": user.role})
-    return {"access_token": access_token, "token_type": "bearer", "role": user.role}
+    user_data = {
+        "id": user.id,
+        "username": user.username,
+        "email": user.email,
+        "role": user.role
+    }
+    return {"access_token": create_access_token(user_data), "token_type": "bearer"}
     
